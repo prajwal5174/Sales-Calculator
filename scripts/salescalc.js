f3437e64807953ad51ac8fe1a34bddabe9ee3223
Prajwal Gurung <78008609+prajwal5174@users.noreply.github.com>
2021-02-08 19:17:47 -0600

    Update salescalc.js

--- scripts/salescalc.js
+++ scripts/salescalc.js
@@ -6,9 +6,9 @@ function main() {
 
 function calculateSales() {
     Price=parseFloat(txPrice.value)
-    TaxRate=parseFloat(txTaxRate.value)/100 
+    TaxRate=parseFloat(txTaxRate.value)/100 //finding out the tax rate by dividing it by 100
     Tax=Price*TaxRate
-    Total=Price+Tax 
+    Total=Price+Tax //formula to calculate the total price after tax
     spTotal.innerHTML=Total.toFixed(2)
 }
 
